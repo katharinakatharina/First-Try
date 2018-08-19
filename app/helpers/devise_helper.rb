035f1bd35545a3a4f4a1ef40e50c346e35abf832
katharinakatharina <katharina.gottwald@gmx.de>
2018-08-19 12:12:25 +0200

    Fixes

---
+++ app/helpers/devise_helper.rb
@@ -0,0 +1,28 @@
+module DeviseHelper
+  def devise_error_messages!
+    return "" unless devise_error_messages?
+
+    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+    sentence = I18n.t("errors.messages.not_saved",
+                      :count => resource.errors.count,
+                      :resource => resource.class.model_name.human.downcase)
+
+    html = <<-HTML
+
+      <div id="error_explanation">
+
+          <div class="error-title"><h2>Watch out!<br>#{sentence}</h2></div>
+          <ul>#{messages}</ul>
+
+      </div>
+
+    HTML
+
+    html.html_safe
+  end
+
+  def devise_error_messages?
+    !resource.errors.empty?
+  end
+
+end
