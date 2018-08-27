71d7b195fae63f429fdb059dfcf0a2feac39caaa
katharinakatharina <katharina.gottwald@gmx.de>
2018-08-27 16:03:05 +0200

    Fixed Actioncable

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -2,7 +2,7 @@ module ApplicationCable
   class Connection < ActionCable::Connection::Base
     identified_by :current_user
      def connect
-
+       self.current_user = find_verified_user
     end
      def disconnect
 
@@ -18,7 +18,4 @@ module ApplicationCable
       end
     end
 
-    def connect
-      self.current_user = find_verified_user
-    end
  end
